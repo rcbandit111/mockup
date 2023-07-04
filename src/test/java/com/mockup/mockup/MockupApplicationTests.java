1bd8688458cf16819334ce4b8fa5d6a73b676fc1
Plamen Terziev <pterziev@datexcorp.com>
2023-07-04 15:07:17 +0300

    test

---
+++ src/test/java/com/mockup/mockup/MockupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockup.mockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
