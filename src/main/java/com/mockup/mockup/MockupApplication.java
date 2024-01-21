3d9f2ef93591d960d23024e4637efd02f8dff999
Plamen Terziev <pterziev@datexcorp.com>
2024-01-22 00:52:34 +0200

    Updated code

--- src/main/java/com/mockup/mockup/MockupApplication.java
+++ src/main/java/com/mockup/mockup/MockupApplication.java
@@ -2,8 +2,10 @@ package com.mockup.mockup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MockupApplication {
 
 	public static void main(String[] args) {
