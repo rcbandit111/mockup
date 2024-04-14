adff39264f3d5136fc7c47a49a181d4ead3c61e6
Mona Lisa <pterziev@datexcorp.com>
2024-04-14 16:49:53 +0300

    Updated code

--- src/main/java/com/mockup/mockup/MockupApplication.java
+++ src/main/java/com/mockup/mockup/MockupApplication.java
@@ -2,26 +2,25 @@ package com.mockup.mockup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.event.EventListener;
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
-import org.yaml.snakeyaml.Yaml;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
+@EnableFeignClients
+@EnableRetry
+@RefreshScope
+@EnableHystrix
+@EntityScan
 @SpringBootApplication
+@EnableAsync
 public class MockupApplication {
 
 	DiscoveryClient discoveryClient;
