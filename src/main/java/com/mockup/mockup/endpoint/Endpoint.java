2037a4fc8ffeda37b82c479c45d0a9799c0f44ff
Mona Lisa <pterziev@datexcorp.com>
2024-04-24 14:18:23 +0300

    Updated code

--- src/main/java/com/mockup/mockup/endpoint/Endpoint.java
+++ src/main/java/com/mockup/mockup/endpoint/Endpoint.java
@@ -1,15 +1,12 @@
 package com.mockup.mockup.endpoint;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/admin")
 public class Endpoint {
 
-    @PostMapping("/greeting")
+    @GetMapping("/greeting")
     public @ResponseBody String greeting()
     {
         return "test response";
